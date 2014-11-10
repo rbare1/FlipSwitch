ce5c9f5030bbf421b92cbbd4a4a33b7aa9c9fa83
Alana Palmerini <apalmeri@ycp.edu>
2014-11-10 12:04:11 -0500

    updated and commit

--- app/src/main/java/cs481/rbamap/flipswitch/LivingRoom.java
+++ app/src/main/java/cs481/rbamap/flipswitch/LivingRoom.java
@@ -64,7 +64,7 @@ public class LivingRoom extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-       // getMenuInflater().inflate(R.menu.living_room, menu);
+        getMenuInflater().inflate(R.menu.living_room, menu);
         return true;
     }
 
